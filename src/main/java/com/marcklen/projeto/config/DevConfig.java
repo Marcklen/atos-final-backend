b582ba569b740952a3314f7b88a13ea721b12aad
Marcklen <marcklen@icloud.com>
2022-11-28 22:52:13 -0300

    perfil de desenvolvimento criado com banco mysql

---
+++ src/main/java/com/marcklen/projeto/config/DevConfig.java
@@ -0,0 +1,21 @@
+package com.marcklen.projeto.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.marcklen.projeto.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciarDB();
+	}
+}
