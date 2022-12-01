c553a858812219b0d8ac6d20122d807228ef4ac5
Marcklen <marcklen@icloud.com>
2022-12-01 13:01:15 -0300

    endpoint findByID para chamados

---
+++ src/main/java/com/marcklen/projeto/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.marcklen.projeto.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcklen.projeto.domain.Chamado;
+import com.marcklen.projeto.repositories.ChamadoRepository;
+import com.marcklen.projeto.services.exceptions.ObjectNotFoundExceptions;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundExceptions("Chamado de Numero " + id+ " não encontrado "));
+	}
+}
